50c2de0223566099513d5fcb92ce3c8d394d21d4
Kristian Storvoll <kristianstorvoll@gmail.com>
2017-10-07 22:00:50 +0200

    added viewpager and base layout for items in onboarding

--- app/src/main/java/no/kristo/foosballcompanion/ui/OnboardingActivity.kt
+++ app/src/main/java/no/kristo/foosballcompanion/ui/OnboardingActivity.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.support.v4.view.PagerAdapter
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_onboarding.*
 import no.kristo.foosballcompanion.R
+import no.kristo.foosballcompanion.ui.view.OnboardingItem
+import no.kristo.foosballcompanion.ui.view.OnboardingItemView
 
 /**
+ * Displays some short information and a sign-up page at the end
  * Created by Kristian on 07.10.2017.
  */
-
-
 class OnboardingActivity : AppCompatActivity() {
 
-    var adapter: PagerAdapter? = null
+    var adapter: OnboardingAdapter = OnboardingAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,19 +29,36 @@ class OnboardingActivity : AppCompatActivity() {
      * Setup the onboarding viewpager
      */
     private fun setupViewPager() {
-        adapter = OnboardingAdapter();
+        //generate the views
+        adapter.views.addAll(generateOnboardingViews())
+        adapter.notifyDataSetChanged()
+
+        //set the adapter
         viewPager.adapter = adapter
+    }
+
+    private fun generateOnboardingViews(): Collection<View> {
+        val views = ArrayList<View>()
+
+        OnboardingItemView(this).also { it.renderOnboardingItem(OnboardingItem("Test", "Test desc" ,null))}.also { adapter.views.add(it) }
+
+        return views
     }
 }
 
 class OnboardingAdapter(val views: ArrayList<View> = ArrayList<View>()) : PagerAdapter() {
 
     override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
-        return true
+        return view == `object`
     }
 
     override fun getCount(): Int {
         return views.count()
+    }
+
+    override fun instantiateItem(container: ViewGroup?, position: Int) {
+        val view = views.get(position)
+        container?.addView(view)
     }
 
 
---
+++ app/src/main/java/no/kristo/foosballcompanion/ui/view/OnboardingItemView.kt
@@ -0,0 +1,37 @@
+package no.kristo.foosballcompanion.ui.view
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import kotlinx.android.synthetic.main.view_onboarding_item.view.*
+import kotterknife.bindView
+import no.kristo.foosballcompanion.R
+
+
+/**
+ * Data class descrigin the onboarding item
+ */
+data class OnboardingItem(val title: String = "", val description: String = "", val image: Drawable?)
+
+/**
+ * View rendering a OnboardingItem
+ * Created by Kristian on 07.10.2017.
+ */
+class OnboardingItemView : LinearLayout {
+
+    constructor(context: Context) : super(context) {
+        internalInit()
+    }
+
+    fun internalInit() {
+        inflate(context, R.layout.view_onboarding_item, this)
+    }
+
+    fun renderOnboardingItem(item: OnboardingItem) {
+        onboarding_item_title.text = item.title
+        onboarding_item_description.text = item.description
+        onboarding_item_image.setImageDrawable(item.image)
+    }
+}
